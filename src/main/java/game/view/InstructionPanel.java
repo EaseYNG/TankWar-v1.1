8c938199b0497bb021261eaa4b7311153563b2f9
Void Yang <2604978804@qq.com>
2025-04-30 18:42:06 +0800

    2025.4.30 18:41

--- src/main/java/game/view/InstructionPanel.java
+++ src/main/java/game/view/InstructionPanel.java
@@ -1,5 +1,21 @@
 package main.java.game.view;
 
-public class InstructionPanel extends APanel {
+import javax.swing.*;
 
+public class InstructionPanel extends APanel {
+    private JLabel label1 = new JLabel("WASD - UP LEFT DOWN RIGHT");
+    private JLabel label2 = new JLabel("SPACE - shoot");
+    private JLabel label3 = new JLabel("R - reload");
+    private JLabel label4 = new JLabel("There are three tank types: Heavy, Medium, Light");
+    private JLabel label5 = new JLabel("Different types means different health, attack, speed");
+    private JLabel label6 = new JLabel("There are three types of difficulties, means three different enemy quantities");
+
+    private BoxLayout layout;
+
+    public InstructionPanel() {
+        layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(layout);
+
+
+    }
 }
